a3a76a8ab318c2b7b272956c81ca81e05b09a54d
Mehran Shahani <mehranshahani6@gmail.com>
2025-04-06 21:47:17 +0330

    Fix: Minor bug fix in retroSlice

--- src/store/slices/retroSlice.ts
+++ src/store/slices/retroSlice.ts
@@ -5,11 +5,13 @@ import { createSlice } from "@reduxjs/toolkit"
 interface RetroState {
     isRetro: boolean
     isRetroPending: boolean
+    shake: boolean
 }
 
 const initialState: RetroState = {
     isRetro: false,
     isRetroPending: false,
+    shake: false,
 }
 
 const retroSlice = createSlice({
@@ -18,10 +20,12 @@ const retroSlice = createSlice({
     reducers: {
         startRetroTransition: (state) => {
             state.isRetroPending = true
+            state.shake = true
         },
         activateRetro: (state) => {
             state.isRetro = true
             state.isRetroPending = false
+            state.shake = false
         },
         deactivateRetro: (state) => {
             state.isRetro = false
